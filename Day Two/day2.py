b7652af5dbc9b9bddcfbb24b14dd095a99e53f2b
Nicholas <nicholaseovva@csus.edu>
2025-12-03 21:39:58 -0800

    solve: day 2 part 2

--- Day Two/day2.py
+++ Day Two/day2.py
@@ -12,22 +12,26 @@ def get_ranges(parsed_ids):
     for id in parsed_ids:
         bounds = id.split("-")
         lower_bound, upper_bound = int(bounds[0]), int(bounds[1])
-        
         for number in range(lower_bound, upper_bound + 1):
-            if len(str(number)) % 2 == 0:
-                ranges.append(number)
+            ranges.append(number)
     return ranges
 
+def is_invalid(number):
+    len_num = len(number)
+    mid = len_num // 2
+      
+    for curr_len in range(1, mid + 1):
+        prefix = number[:curr_len]
+        if prefix * (len_num // curr_len) == number:
+            return True
+    return False
+        
 def find_invalid_ids(ranges):
     sum_of_invalids = 0
     for number in ranges:
-        number = str(number)
-        mid = len(str(number)) // 2
-        if number[:mid] == number[mid:]:
-            sum_of_invalids += int(number)
+        if is_invalid(str(number)):
+            sum_of_invalids += number
     return sum_of_invalids
-
-
 
 if __name__ == "__main__":
     parsed_ids = parse_ids()
